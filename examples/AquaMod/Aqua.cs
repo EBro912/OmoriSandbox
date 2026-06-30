73228889b01dfdbdce362ad11abcc97674a5f32d
Ethan <ebrogames@gmail.com>
2026-06-29 23:19:03 -0400

    Add AquaMod example

---
+++ examples/AquaMod/Aqua.cs
@@ -0,0 +1,177 @@
+using Godot;
+using OmoriSandbox;
+using OmoriSandbox.Actors;
+using OmoriSandbox.Battle;
+using OmoriSandbox.Modding;
+
+namespace AquaMod;
+
+public class Aqua : Enemy
+{
+    public override string Name => "AQUA";
+    public override SpriteFrames Animation => new SpriteFramesBuilder("AquaMod/sprites/aqua.png", 152, 152)
+        .AddAnimation("neutral", 5, 0, 1, 2, 3, 4, 5, 6, 7)
+        .AddAnimation("laugh", 5, 8, 9)
+        .AddAnimation("hurt", 1, 10)
+        .AddAnimation("pose", 1, 11)
+        .AddAnimation("spin", 5, 12, 13, 14, 15, 16)
+        .AddAnimation("dance", 5, 18, 17, 18, 19)
+        .AddAnimation("chain", 5, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29)
+        .AddAnimation("toast", 1, 30)
+        .AddAnimation("amused", 1, 31)
+        .Build();
+
+    protected override Stats Stats => new(3060, 0, 16, 0, 20, 10, 40);
+    protected override string[] EquippedSkills => ["AQKnifeFan", "AQKnifeChain", "AQKnifeCircle", "AQOmega"];
+
+    public override bool IsStateValid(string state)
+    {
+        return state is "neutral" or "toast";
+    }
+
+    private int Mercy = 0;
+    private bool Attacked = false;
+    private int Turn = 0;
+    private HashSet<string> UsedActs = [];
+    private int Duplicates = 0;
+    private bool UsedOmega = false;
+
+    public void AddMercy(string act)
+    {
+        if (UsedActs.Contains(act)) return;
+        AudioManager.Instance.PlaySFX("mercy_add", 1, 2);
+        Mercy += 25;
+        BattleManager.Instance.SpawnDamageNumber(25, CenterPoint, DamageType.JuiceGain);
+    }
+
+    public async Task DoACTDialogue(string act)
+    {
+        if (!UsedActs.Add(act))
+        {
+            Duplicates++;
+            switch (Duplicates)
+            {
+                case 1:
+                    DialogueManager.Instance.QueueMessage("Didn't we play that old game already?");
+                    DialogueManager.Instance.QueueMessage("Let's do something else!");
+                    break;
+                case 2:
+                    DialogueManager.Instance.QueueMessage("What a dull game.");
+                    DialogueManager.Instance.QueueMessage("Wouldn't you rather play with something sharp!?");
+                    break;
+                default:
+                    DialogueManager.Instance.QueueMessage( @"Hum,\. is that all you can do?");
+                    DialogueManager.Instance.QueueMessage(@"After all,\. humans are actually a little boring!");
+                    break;
+            }
+            await DialogueManager.Instance.WaitForDialogue();
+            return;
+        }
+
+        switch (act)
+        {
+            case "pose":
+                DialogueManager.Instance.QueueMessage(this, "Oh, I get it! Posey, posey!");
+                DialogueManager.Instance.QueueMessage(this, "Are we part of a team now too!?");
+                break;
+            case "spin":
+                DialogueManager.Instance.QueueMessage(this, @"Hee hee! What's going on?\. Is the whole world revolving!?");
+                break;
+            case "care":
+                DialogueManager.Instance.QueueMessage(this, @"HAHA! What is that!?\. Can humans change their shape so!?");
+                await DialogueManager.Instance.WaitForDialogue();
+                ForceState("toast");
+                DialogueManager.Instance.QueueMessage(this, "...");
+                await DialogueManager.Instance.WaitForDialogue();
+                ForceState("amused");
+                DialogueManager.Instance.QueueMessage(this, "I knew that!!");
+                break;
+            case "dance":
+                DialogueManager.Instance.QueueMessage(this, @"Haha!\. Your feet are making such strange music!");
+                break;
+        }
+        await DialogueManager.Instance.WaitForDialogue();
+        DialogueManager.Instance.QueueMessage($"{Name} is now at {Mercy}% mercy.");
+        await DialogueManager.Instance.WaitForDialogue();
+    }
+    
+    public override async Task ProcessBattleConditions()
+    {
+        if (UsedOmega)
+        {
+            ForceState("laugh");
+            DialogueManager.Instance.QueueMessage(this, @"Uee hee hee!! Now, your turn,\. your turn!!");
+            DialogueManager.Instance.QueueMessage(this, "Magic, magic, Omega, magic!");
+            await DialogueManager.Instance.WaitForDialogue();
+            await Wait.Seconds(3);
+            ForceState("toast");
+            AudioManager.Instance.StopBGM();
+            DialogueManager.Instance.QueueMessage(this, "...what?");
+            DialogueManager.Instance.QueueMessage(this, "You can't do it!?");
+            DialogueManager.Instance.QueueMessage(this, @"Uuu,\. how boring...");
+            DialogueManager.Instance.QueueMessage(this, "No more battle!");
+            await DialogueManager.Instance.WaitForDialogue();
+            CurrentHP = 0;
+            return;
+        }
+        
+        if (!Attacked && CurrentHP < 3060)
+        {
+            AudioManager.Instance.PlaySFX("mercy_add", 1, 2);
+            Mercy += 25;
+            BattleManager.Instance.SpawnDamageNumber(25, CenterPoint, DamageType.JuiceGain);
+            DialogueManager.Instance.QueueMessage(this, @"Uuu, what is this!?\. 'Pain'?\. How funny!!");
+            DialogueManager.Instance.QueueMessage(this, @"Did you know?\. You should try some too!");
+            await DialogueManager.Instance.WaitForDialogue();
+            DialogueManager.Instance.QueueMessage($"AQUA is now at {Mercy}% mercy.");
+            await DialogueManager.Instance.WaitForDialogue();
+            Attacked = true;
+        }
+
+        if (CurrentHP < 612)
+        {
+            DialogueManager.Instance.QueueMessage(this, @"Uuu, this is fun,\. fun!!!");
+            DialogueManager.Instance.QueueMessage("SETH", @"Wh-\.what are you doing!?");
+            DialogueManager.Instance.QueueMessage(this, @"Playing a game!\. Do you want to play, too!?");
+            DialogueManager.Instance.QueueMessage("SETH", @"What!?\. NO!!\. If you keep fighting, you'll end up...");
+            DialogueManager.Instance.QueueMessage(this, @"Uuu? What, what? End up what?\. Is it another type of game...?");
+            DialogueManager.Instance.QueueMessage("SETH", "N-No, I'm just worried that...");
+            await DialogueManager.Instance.WaitForDialogue();
+            ForceState("laugh");
+            DialogueManager.Instance.QueueMessage(this, "Don't worry, you can play too!!");
+            DialogueManager.Instance.QueueMessage("SETH", "That's not what I'm saying!!! Just give up!! I'm going to retreat, too!");
+            await DialogueManager.Instance.WaitForDialogue();
+            ForceState("amused");
+            DialogueManager.Instance.QueueMessage(this, "Uuu, okay...");
+            await DialogueManager.Instance.WaitForDialogue();
+            CurrentHP = 0;
+            return;
+        }
+
+        if (Mercy >= 100)
+        {
+            DialogueManager.Instance.QueueMessage(this, @"Uee hee hee,\. this is all so much fun!");
+            DialogueManager.Instance.QueueMessage(this, @"Here,\. I'll do something,\. and you copy!");
+            DialogueManager.Instance.QueueMessage(this, @"My magic,\. my Omega,\. can you follow?");
+            await DialogueManager.Instance.WaitForDialogue();
+            BattleManager.Instance.ForceCommand(this, SelectAllTargets(), Skills["AQOmega"]);
+            UsedOmega = true;
+        }
+    }
+
+    public override Task ProcessStartOfTurn()
+    {
+        Turn++;
+        return Task.CompletedTask;
+    }
+
+    public override BattleCommand ProcessAI()
+    {
+        return (Turn % 3) switch
+        {
+            1 => new BattleCommand(this, SelectTarget(), Skills["AQKnifeFan"]),
+            2 => new BattleCommand(this, SelectTarget(), Skills["AQKnifeChain"]),
+            _ => new BattleCommand(this, SelectTarget(), Skills["AQKnifeCircle"])
+        };
+    }
+}
---
+++ examples/AquaMod/AquaMod.cs
@@ -0,0 +1,204 @@
+﻿using Godot;
+using OmoriSandbox;
+using OmoriSandbox.Animation;
+using OmoriSandbox.Battle;
+using OmoriSandbox.Modding;
+
+namespace AquaMod;
+
+public partial class AquaMod : Mod
+{
+    public override void OnLoad()
+    {
+        RegisterEnemy<Aqua>("Aqua");
+        
+        RegisterSkill("AQKnifeChain", new Skill(
+            name: "AQKnifeChain",
+            description: "AQKnifeChain",
+            target: SkillTarget.Enemy,
+            cost: 0,
+            hidden: true,
+            effect: async (self, target) =>
+            {
+                BattleLogManager.Instance.QueueMessage(self, "[actor] throws a chain of knives!");
+                if (self is Aqua)
+                    self.ForceState("chain");
+                Tween tween;
+                for (int i = 0; i < 6; i++)
+                {
+                    tween = CreateTween();
+                    tween.TweenProperty(self.Sprite, "offset",
+                        new Vector2(GameManager.Instance.Random.RandiRange(-50, 50),
+                            GameManager.Instance.Random.RandiRange(-50, 50)), 0.2f).SetTrans(Tween.TransitionType.Sine).SetEase(Tween.EaseType.InOut);
+                    await AnimationManager.Instance.WaitForAnimation(20, target);
+                    BattleManager.Instance.Damage(self, target, () => self.CurrentStats.ATK * 5 - target.CurrentStats.DEF, false, 0.1f, neverCrit: true);
+                }
+                tween = CreateTween();
+                tween.TweenProperty(self.Sprite, "offset", Vector2.Zero, 0.2f).SetTrans(Tween.TransitionType.Sine).SetEase(Tween.EaseType.InOut);
+                await ToSignal(tween, Tween.SignalName.Finished);
+                self.ForceState("neutral");
+            }
+        ));
+        
+        RegisterSkill("AQKnifeFan", new Skill(
+            name: "AQKnifeFan",
+            description: "AQKnifeFan",
+            target: SkillTarget.Enemy,
+            cost: 0,
+            hidden: true,
+            effect: async (self, target) =>
+            {
+                BattleLogManager.Instance.QueueMessage(self, "[actor] throws a fan of knives!");
+                await Wait.Milliseconds(250);
+                await AnimationManager.Instance.WaitForAnimation(6, target);
+                for (int i = 0; i < 4; i++)
+                {
+                    BattleManager.Instance.Damage(self, target, () => self.CurrentStats.ATK * 5 - target.CurrentStats.DEF, false, 0.1f, neverCrit: true);
+                }
+            }
+        ));
+        
+        RegisterSkill("AQKnifeCircle", new Skill(
+            name: "AQKnifeCircle",
+            description: "AQKnifeCircle",
+            target: SkillTarget.Enemy,
+            cost: 0,
+            hidden: true,
+            effect: async (self, target) =>
+            {
+                BattleLogManager.Instance.QueueMessage(self, "[actor] throws knives in a circle!");
+                await Wait.Milliseconds(500);
+                for (int i = 0; i < 12; i++)
+                {
+                    await AnimationManager.Instance.WaitForAnimation(i % 2 == 0 ? 9 : 20, target);
+                    BattleManager.Instance.Damage(self, target, () => self.CurrentStats.ATK * 5 - target.CurrentStats.DEF, false, 0.1f, neverCrit: true);
+                }
+            }
+        ));
+        
+        RegisterSkill("AQOmega", new Skill(
+            name: "AQOmega",
+            description: "AQOmega",
+            target: SkillTarget.AllEnemies,
+            cost: 0,
+            hidden: true,
+            effect: async (self, target) =>
+            {
+                BattleLogManager.Instance.QueueMessage(self, "[actor] uses her OMEGA attack!");
+                await Wait.Milliseconds(1000);
+                AnimationManager.Instance.PlayAnimation(13, target);
+                for (int i = 0; i < 10; i++)
+                {
+                    BattleManager.Instance.Damage(self, target, () => self.CurrentStats.ATK * 5 - target.CurrentStats.DEF, false, 0.1f, neverCrit: true);
+                    await Wait.Milliseconds(350);
+                }
+            }
+        ));
+
+        RegisterSkill("CoolPose", new Skill(
+            name: "COOLPOSE",
+            description: "25% mercy",
+            target: SkillTarget.Enemy,
+            cost: 0,
+            effect: async (self, target) =>
+            {
+                BattleLogManager.Instance.QueueMessage(self, "[actor] struck a cool pose!");
+                AnimationManager.Instance.PlayAnimation(148, self);
+                if (target is Aqua enemy)
+                {
+                    enemy.AddMercy("pose");
+                    target.ForceState("pose");
+                    await Wait.Milliseconds(1000);
+                    BattleLogManager.Instance.QueueMessage("... the enemy posed back!");
+                    await Wait.Milliseconds(1000);
+                    await enemy.DoACTDialogue("pose");
+                    target.ForceState("neutral");
+                }
+                else
+                {
+                    BattleLogManager.Instance.QueueMessage("...but nothing happened.");
+                }
+            }
+        ));
+        
+        RegisterSkill("Spin", new Skill(
+            name: "SPIN",
+            description: "25% mercy",
+            target: SkillTarget.Enemy,
+            cost: 0,
+            effect: async (self, target) =>
+            {
+                BattleLogManager.Instance.QueueMessage(self, "[actor] spun in place!");
+                AnimationManager.Instance.PlayAnimation(72, self);
+                if (target is Aqua enemy)
+                {
+                    enemy.AddMercy("spin");
+                    target.ForceState("spin");
+                    await Wait.Milliseconds(1000);
+                    BattleLogManager.Instance.QueueMessage("... the enemy spun around, too!");
+                    await Wait.Milliseconds(1000);
+                    await enemy.DoACTDialogue("spin");
+                    target.ForceState("neutral");
+                }
+                else
+                {
+                    BattleLogManager.Instance.QueueMessage("...but nothing happened.");
+                }
+            }
+        ));
+        
+        RegisterSkill("Care", new Skill(
+            name: "CARE",
+            description: "25% mercy",
+            target: SkillTarget.Enemy,
+            cost: 0,
+            effect: async (self, target) =>
+            {
+                BattleLogManager.Instance.QueueMessage(self, "[actor] showed tender loving care!");
+                AnimationManager.Instance.PlayAnimation(212, self);
+                if (target is Aqua enemy)
+                {
+                    enemy.AddMercy("laugh");
+                    target.ForceState("laugh");
+                    await Wait.Milliseconds(1000);
+                    BattleLogManager.Instance.QueueMessage("... the enemy laughed!");
+                    await Wait.Milliseconds(1000);
+                    await enemy.DoACTDialogue("care");
+                    target.ForceState("neutral");
+                }
+                else
+                {
+                    BattleLogManager.Instance.QueueMessage("...but nothing happened.");
+                }
+            }
+        ));
+        
+        RegisterSkill("Dance", new Skill(
+            name: "DANCE",
+            description: "25% mercy",
+            target: SkillTarget.Enemy,
+            cost: 0,
+            effect: async (self, target) =>
+            {
+                BattleLogManager.Instance.QueueMessage(self, "[actor] danced with enthusiasm!");
+                AudioManager.Instance.PlaySFX("BA_do_nothing_dance", 1, 0.9f);
+                if (target is Aqua enemy)
+                {
+                    enemy.AddMercy("dance");
+                    target.ForceState("dance");
+                    await Wait.Milliseconds(1000);
+                    BattleLogManager.Instance.QueueMessage("... the enemy danced, too!");
+                    await Wait.Milliseconds(1000);
+                    await enemy.DoACTDialogue("dance");
+                    target.ForceState("neutral");
+                }
+                else
+                {
+                    BattleLogManager.Instance.QueueMessage("...but nothing happened.");
+                }
+            }
+        ));
+        
+        GD.Print("AquaMod loaded!");
+    }
+}
